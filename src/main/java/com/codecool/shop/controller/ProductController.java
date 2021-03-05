6348605b55be61287e63822817ed9aafc2f2bd06
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-05 19:52:25 +0200

    Simplify processIdInput() in ProductController

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,11 +1,11 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
@@ -19,9 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})        // localhost/?sort-category=2
 public class ProductController extends HttpServlet {
@@ -53,15 +51,15 @@ public class ProductController extends HttpServlet {
         ProductCategory selectedCategory = productCategoryDataStore.find(categoryId);
         Supplier selectedSupplier = supplierDataStore.find(supplierId);
 
-        List<Product> productsByCategory = displayFilteredByCategory(categoryId,productDataStore,selectedCategory);
-        List<Product> productsBySupplier = displayFilteredBySupplier(supplierId,productDataStore,selectedSupplier);
+        List<Product> productsByCategory = displayFilteredByCategory(categoryId, productDataStore, selectedCategory);
+        List<Product> productsBySupplier = displayFilteredBySupplier(supplierId, productDataStore, selectedSupplier);
 
         productsByCategory.retainAll(productsBySupplier);
 
         List<Product> finalList = productsByCategory;
 
 
-                TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("categories", productCategoryDataStore.getAll());
 //        context.setVariable("category", productCategoryDataStore.find(categoryId));
@@ -94,21 +92,17 @@ public class ProductController extends HttpServlet {
         // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         // context.setVariables(params);
         engine.process("product/index.html", context, resp.getWriter());
-        }
+    }
 
 
     private int processIdInput(String categoryIdAsString) {
-        int categoryId;
-        if (categoryIdAsString == null) {
-            categoryId = -1;
-        } else {
-            try {
-                categoryId = Integer.parseInt(categoryIdAsString);
-            } catch (Exception e) {
-                categoryId = -1;
-            }
+        int categoryId = -1;
+        try {
+            categoryId = Integer.parseInt(categoryIdAsString);
+        } catch (Exception ignored) {
+
         }
-        
+
         return categoryId;
     }
 
@@ -120,7 +114,7 @@ public class ProductController extends HttpServlet {
         }
     }
 
-    private List<Product> displayFilteredBySupplier(int idSupplier, ProductDao productDataStore, Supplier supplier){
+    private List<Product> displayFilteredBySupplier(int idSupplier, ProductDao productDataStore, Supplier supplier) {
         if (idSupplier == -1) {
             return productDataStore.getAll();
         } else {
