a4850cb15c1e962e75cc512c2c435d567e9c407d
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-05 05:45:03 +0200

    Add deserializer for List<LineItem>

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -2,6 +2,14 @@ package com.codecool.shop.model;
 
 import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.dao.ProductDao;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
 
 public class LineItem extends SimpleLineItem{
     private float productPrice;
@@ -11,7 +19,8 @@ public class LineItem extends SimpleLineItem{
         this.productPrice = productPrice;
     }
 
-    public LineItem(int productId, int quantity) {
+    @JsonCreator
+    public LineItem(@JsonProperty("productId")int productId, @JsonProperty("quantity")int quantity) {
         super(productId, quantity);
         this.productPrice = 0.0f;
         updateProductPrice();
@@ -30,5 +39,17 @@ public class LineItem extends SimpleLineItem{
         return productPrice;
     }
 
+    public static List<LineItem> deserializeListFromString(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        List<LineItem> deserializedList = null;
+        try {
+            deserializedList = objectMapper.readValue(json, new TypeReference<List<LineItem>>() {});
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return deserializedList;
+    }
 
 }
