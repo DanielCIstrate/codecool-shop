60f35c59def0f1488712bb01dd7f52fe8886725c
DanielCIstrate <daniel.istr8@gmail.com>
2021-03-18 13:22:03 +0200

    added increment/decrement buttons
    values are stored directly to the local storage JSON

--- src/main/webapp/static/js/view-cart.js
+++ src/main/webapp/static/js/view-cart.js
@@ -1,10 +1,47 @@
 import {dataHandler} from "/static/js/data_handler.js";
 
 window.addEventListener('load', function(event) {
+
+    function increment(triggerButton) {
+      let element = triggerButton.parentElement.firstElementChild;
+      element.value =
+          parseInt(element.value) + 1;
+      let productIdForButton = parseInt(element.dataset.productId);
+      dataHandler.addLineItemInOrderJSON(productIdForButton, 1);
+   }
+
+   function decrement(triggerButton) {
+      let element = triggerButton.parentElement.firstElementChild;
+      if (parseInt(element.value) >= 1) {
+         element.value =
+             parseInt(element.value) - 1;
+         let productIdForButton = parseInt(element.dataset.productId);
+         dataHandler.addLineItemInOrderJSON(productIdForButton, -1);
+      }
+   }
+
    dataHandler.init();
+
+   let incrementButtons = document.querySelectorAll(".incrementButton");
+   let decrementButtons = document.querySelectorAll(".decrementButton");
+
+   decrementButtons.forEach(button => {
+           button.addEventListener(
+               'click',
+               (event) => decrement(event.target)
+           )
+       });
+
+   incrementButtons.forEach(button => {
+              button.addEventListener(
+                  'click',
+                  (event) => increment(event.target)
+              )
+          });
+
 }, false);
 
-let data = dataHandler.getSessionItem("data");
-if (data.hasOwnProperty("orderId")) {
+// let data = dataHandler.getSessionItem("data");
 
-}
+
+
