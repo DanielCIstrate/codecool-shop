f2218d89c47ccbd7ca2df84ffd6aca706fdcd8ae
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-19 13:11:52 +0200

    Initial ProductDaoJdbc
    Not all queries(methods) written yet!

---
+++ src/main/java/com/codecool/shop/dao/implementation/sql/ProductDaoJdbc.java
@@ -0,0 +1,76 @@
+package com.codecool.shop.dao.implementation.sql;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+
+public class ProductDaoJdbc implements ProductDao {
+    private DataSource dataSource;
+    public ProductDaoJdbc(DataSource newDataSource) {
+        this.dataSource = newDataSource;
+    }
+
+    @Override
+    public void add(Product product) {
+        try(Connection connectionObject = dataSource.getConnection()) {
+            String sqlQuery = "INSERT INTO product (" +
+                    "name, description, default_price, default_currency, category_id, supplier_id" +
+                    ") VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement precompiledQuery = connectionObject.prepareStatement(
+                    sqlQuery,
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+
+            precompiledQuery.setString(1, product.getName());
+            precompiledQuery.setString(2, product.getDescription());
+            precompiledQuery.setFloat(3, product.getDefaultPrice());
+            precompiledQuery.setString(4, "USD");
+            precompiledQuery.setInt(5, product.getProductCategory().getId());
+            precompiledQuery.setInt(6, product.getSupplier().getId());
+            precompiledQuery.executeUpdate();
+
+            ResultSet resultCursor = precompiledQuery.getGeneratedKeys();
+            resultCursor.next();
+            product.setId(resultCursor.getInt(1));
+
+        } catch (SQLException error) {
+            throw new RuntimeException("Error while adding new Author.", error);
+        }
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getIntersection(ProductCategory someCategory, Supplier someSupplier) {
+        return null;
+    }
+}
