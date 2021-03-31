e6dd9d98c0dd2596d8f26fde560bdb74fc8a1153
DanielCIstrate <daniel.istr8@gmail.com>
2021-03-31 10:36:10 +0300

    add find methods needed in ProductDaoJDBC

--- src/main/java/com/codecool/shop/dao/implementation/sql/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/sql/ProductCategoryDaoJdbc.java
@@ -2,8 +2,13 @@ package com.codecool.shop.dao.implementation.sql;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ProductCategoryDaoJdbc implements ProductCategoryDao {
@@ -19,8 +24,34 @@ public class ProductCategoryDaoJdbc implements ProductCategoryDao {
     }
 
     @Override
-    public ProductCategory find(int id) {
-        return null;
+    public ProductCategory find(int id) throws IllegalArgumentException {
+        String currentName;
+        String currentDescription;
+        String currentDepartment;
+        try (Connection connectionObject = dataSource.getConnection()) {
+
+            String sqlQuery = "SELECT name, description,department FROM category " +
+                    "WHERE (id = ?)";
+            PreparedStatement precompiledQuery = connectionObject.prepareStatement(sqlQuery);
+            precompiledQuery.setInt(1, id);
+            precompiledQuery.executeQuery();
+            ResultSet resultSet = precompiledQuery.getGeneratedKeys();
+            if (!resultSet.next()) {
+                throw new IllegalArgumentException("Could not find product category id = " + id);
+            }
+            else {
+                currentName = resultSet.getString(1);
+                currentDescription = resultSet.getString(2);
+                currentDepartment = resultSet.getString(3);
+            }
+
+
+        } catch (SQLException error) {
+            throw new RuntimeException("Error while attempting to get Product with id="+id+" from DB!");
+        }
+
+        ProductCategory foundCategory = new ProductCategory(currentName,currentDepartment,currentDescription);
+        return foundCategory;
     }
 
     @Override
--- src/main/java/com/codecool/shop/dao/implementation/sql/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/sql/SupplierDaoJdbc.java
@@ -4,6 +4,10 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SupplierDaoJdbc implements SupplierDao {
@@ -18,8 +22,33 @@ public class SupplierDaoJdbc implements SupplierDao {
     }
 
     @Override
-    public Supplier find(int id) {
-        return null;
+    public Supplier find(int id) throws IllegalArgumentException {
+        String currentName;
+        String currentDescription;
+        try (Connection connectionObject = dataSource.getConnection()) {
+
+            String sqlQuery = "SELECT name, description FROM supplier " +
+                    "WHERE (id = ?)";
+            PreparedStatement precompiledQuery = connectionObject.prepareStatement(sqlQuery);
+            precompiledQuery.setInt(1, id);
+            precompiledQuery.executeQuery();
+            ResultSet resultSet = precompiledQuery.getGeneratedKeys();
+            if (!resultSet.next()) {
+                throw new IllegalArgumentException("Could not find supplier id = " + id);
+            }
+            else {
+                currentName = resultSet.getString(1);
+                currentDescription = resultSet.getString(2);
+            }
+
+
+        } catch (SQLException error) {
+            throw new RuntimeException("Error while attempting to get Product with id="+id+" from DB!");
+        }
+
+        Supplier foundSupplier = new Supplier(currentName, currentDescription);
+        foundSupplier.setId(id);
+        return foundSupplier;
     }
 
     @Override
